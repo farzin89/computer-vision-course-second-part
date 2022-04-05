9f2ceb3e443e33dd66c38867a1dcb7d2848b4791
farzin89 <farzin_ahmadi90@yahoo.com>
2022-04-05 18:49:48 +0200

    Face Detection with open CV

--- 06 - Object detection/Face Detection with open CV.py
+++ 06 - Object detection/Face Detection with open CV.py
@@ -10,8 +10,79 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-nadia = cv2.imread('Nadia.jpgad',0)
+nadia = cv2.imread('nadia.PNG',0)
 denis = cv2.imread('Denis.jpg',0)
 solvay = cv2.imread('Solvay_conference2.jpg',0)
 
-plt.imshow(solvay,cmap='gray')
+#plt.imshow(nadia,cmap='gray')
+#plt.show()
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+def detect_face (img):
+
+     face_img = img.copy()
+     face_rects = face_cascade.detectMultiScale(face_img)
+
+     for (x,y,w,h) in face_rects:
+         cv2.rectangle(face_img,(x,y),(x+w,y+h),(255,255,255),10)
+
+     return face_img
+
+result = detect_face(solvay)
+#plt.imshow(result,cmap = 'gray')
+#plt.show()
+# sometime detect wrongly in picture which has many people
+# in order to solve that we are going to add few parameters
+
+def adj_detect_face(img):
+    face_img = img.copy()
+    face_rects = face_cascade.detectMultiScale(face_img,scaleFactor=1.2,minNeighbors=5)
+
+    for (x, y, w, h) in face_rects:
+        cv2.rectangle(face_img, (x, y), (x + w, y + h), (255, 255, 255), 10)
+
+    return face_img
+#result = adj_detect_face(solvay)
+#plt.imshow(result,cmap = 'gray')
+plt.show()
+
+# eye detection
+
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+def detect_eyes(img):
+
+    face_img = img.copy()
+    eyes_rects = eye_cascade.detectMultiScale(face_img,scaleFactor=1.2,minNeighbors=5)
+
+    for (x, y, w, h) in eyes_rects:
+        cv2.rectangle(face_img, (x, y), (x + w, y + h), (255, 255, 255), 10)
+
+    return face_img
+
+result = detect_eyes(nadia)
+#plt.imshow(result,cmap = 'gray')
+#plt.show()
+
+# detect from camera
+
+cap = cv2.VideoCapture(0)
+
+while True :
+
+    ret,frame = cap.read(0)
+
+    frame = detect_face(frame)
+
+    cv2.imshow('video face detect',frame)
+
+    k = cv2.waitKey(1)
+    if k== 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
